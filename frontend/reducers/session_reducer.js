7ef0f4774164afd0b0b06d4d02b4ed38cbb22a2b
Alexa Bagnard <abagnard@gmail.com>
2016-10-26 10:15:44 -0700

    created root reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,14 +1,4 @@
-// The SessionReducer should listen for 3 action types and respond to each like so:
-//
-// RECEIVE_CURRENT_USER - sets currentUser to the action's user and clears errors
-// RECEIVE_ERRORS - sets errors to the action's errors and clears the currentUser
-// LOGOUT - clears both errors and currentUser
-// Your SessionReducer should maintain its own default state. To do that pass in an object as a default argument to SessionReducer with currentUser set to null and errors set to an empty array.
-//
-// Remember to use both Object.freeze() and merge from the lodash library to prevent the state from being accidentally mutated.
-
-import {SessionConstants} from '../actions/session_actions.js';
-
+import { SessionConstants } from '../actions/session_actions.js';
 import merge from 'lodash/merge';
 
 const _nullUser = Object.freeze({
