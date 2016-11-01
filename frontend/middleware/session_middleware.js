b65f8c32e95ee729cdd1ba3bed367def907803ae
Alexa Bagnard <abagnard@gmail.com>
2016-10-31 17:27:13 -0700

    session middleware created

---
+++ frontend/middleware/session_middleware.js
@@ -0,0 +1,21 @@
+import SessionConstants from '../actions/session_actions';
+import { signup, login, logout } from '../util/session_api_util';
+
+export const SessoinMiddleware = ({getState, dispatch}) => next => action => {
+  const successCallback = user => dispatch(SessionConstants.RECEIVE_CURRENT_USER(user));
+  const errorCallback = xhr => dispatch(SessionConstants.RECEIVE_ERRORS(xhr.responseJSON));
+
+  switch(action.type){
+    case SessionConstants.LOGIN:
+      login(action.user, successCallback, errorCallback);
+      return next(action);
+    case SessionConstants.LOGOUT:
+      logout(() => next(action));
+      break;
+    case SessionConstants.SIGNUP:
+      signup(action.user, successCallback, errorCallback);
+      return next(action);
+    default:
+      return next(action);
+  }
+};
