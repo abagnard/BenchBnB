c02d8adf82fe359e2636b36608a5dace46db2e54
Alexa Bagnard <abagnard@gmail.com>
2016-11-03 16:07:32 -0700

    started session form component

---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+class SessionForm extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = this.state;
+    this.props.processForm({ user });
+  }
+
+  render(){
+    return(
+
+    );
+  }
+
+}
+
+export default SessionForm;
